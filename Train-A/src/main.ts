c3db1775d656a24eb94c22435e0b16d138d6a591
Denis Saveliev <denissaveliev23@gmail.com>
2024-08-16 02:46:55 +0300

    chore: install planess/train-a-backend@latest

--- Train-A/src/main.ts
+++ Train-A/src/main.ts
@@ -5,17 +5,20 @@ import { importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { startServer } from '@planess/train-a-backend';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(BrowserAnimationsModule, BrowserModule),
-    provideStore(),
-    provideStoreDevtools({
-      maxAge: 25,
-      autoPause: true,
-      trace: true,
-      traceLimit: 25,
-      connectInZone: true,
-    }),
-  ],
-}).catch((err) => console.error(err));
+startServer().then(() =>
+  bootstrapApplication(AppComponent, {
+    providers: [
+      importProvidersFrom(BrowserAnimationsModule, BrowserModule),
+      provideStore(),
+      provideStoreDevtools({
+        maxAge: 25,
+        autoPause: true,
+        trace: true,
+        traceLimit: 25,
+        connectInZone: true,
+      }),
+    ],
+  }).catch((err) => console.error(err)),
+);

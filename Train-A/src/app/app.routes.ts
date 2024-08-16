6da9febc0c5fc5485b925e5ec46b42abdd7da8cf
Natalia Bandurova <natacsp@mail.ru>
2024-08-17 00:03:44 +0300

    feat: add auth s,g,i

--- Train-A/src/app/app.routes.ts
+++ Train-A/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./auth/pages/sign-up/sign-up.component').then(
+        (m) => m.SignUpComponent,
+      ),
+  },
+];

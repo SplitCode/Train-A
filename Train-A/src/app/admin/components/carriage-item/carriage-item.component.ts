e3f992b1110424a69a88f00429be4cc41523ad9e
Denis Saveliev <denissaveliev23@gmail.com>
2024-08-20 01:41:16 +0300

    feat: add one size to seats

--- Train-A/src/app/admin/components/carriage-item/carriage-item.component.ts
+++ Train-A/src/app/admin/components/carriage-item/carriage-item.component.ts
@@ -1,16 +1,17 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CarriageItem } from '../../models/carriage-item.interface';
 import { CommonModule } from '@angular/common';
+import { CustomButtonComponent } from '../../../shared/components/custom-button/custom-button.component';
 
 @Component({
   selector: 'app-carriage-item',
   templateUrl: './carriage-item.component.html',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CustomButtonComponent],
   styleUrls: ['./carriage-item.scss'],
 })
 export class CarriageItemComponent implements OnInit {
-  @Input() public config?: CarriageItem;
+  @Input() public config!: CarriageItem;
 
   ngOnInit() {}
 

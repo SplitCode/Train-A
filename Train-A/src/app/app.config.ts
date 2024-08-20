bff40248fab2521a268e036c8ac468b75b4450fe
Denis Saveliev <denissaveliev23@gmail.com>
2024-08-20 11:31:36 +0300

    chore: setup providers

--- Train-A/src/app/app.config.ts
+++ Train-A/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -7,6 +11,12 @@ import { provideEffects } from '@ngrx/effects';
 import { CarriageService } from './admin/services/carriage.service';
 import { CarriageEffects } from './redux/effects/carriage.effects';
 import { UserEffects } from './redux/effects/user.effects';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule, provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { indexReducer, metaReducers } from './redux/reducers/index.reducer';
+import { PRIME_NG_MODULES } from './shared/modules/prime-ng-modules';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +25,19 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors([authInterceptor])),
     CarriageService,
     provideEffects([UserEffects, CarriageEffects]),
+    PRIME_NG_MODULES.MessageService,
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      BrowserModule,
+      StoreModule.forRoot(indexReducer, { metaReducers }),
+    ),
+    provideStore(indexReducer),
+    provideStoreDevtools({
+      maxAge: 25,
+      autoPause: true,
+      trace: true,
+      traceLimit: 25,
+      connectInZone: true,
+    }),
   ],
 };
--- Train-A/src/main.ts
+++ Train-A/src/main.ts
@@ -1,34 +1,10 @@
-import { PRIME_NG_MODULES } from './app/shared/modules/prime-ng-modules';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { provideStore, StoreModule } from '@ngrx/store';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { startServer } from '@planess/train-a-backend';
-import { indexReducer, metaReducers } from './app/redux/reducers/index.reducer';
-
 import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
 startServer().then(() =>
   bootstrapApplication(AppComponent, {
-    providers: [
-      PRIME_NG_MODULES.MessageService,
-      ...appConfig.providers,
-      importProvidersFrom(
-        BrowserAnimationsModule,
-        BrowserModule,
-        StoreModule.forRoot(indexReducer, { metaReducers }),
-      ),
-      provideStore(indexReducer),
-      provideStoreDevtools({
-        maxAge: 25,
-        autoPause: true,
-        trace: true,
-        traceLimit: 25,
-        connectInZone: true,
-      }),
-    ],
+    providers: appConfig.providers,
   }).catch((err) => console.error(err)),
 );

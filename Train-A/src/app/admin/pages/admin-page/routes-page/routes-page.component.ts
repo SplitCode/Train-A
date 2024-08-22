d5f60d387e889fafda8918c088515aa71f3db226
Natalia Bandurova <natacsp@mail.ru>
2024-08-22 13:40:41 +0300

    feat: add admin sidebar

---
+++ Train-A/src/app/admin/pages/admin-page/routes-page/routes-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-routes-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './routes-page.component.html',
+  styleUrl: './routes-page.component.scss',
+})
+export class RoutesPageComponent {}

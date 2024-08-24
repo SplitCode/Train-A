439d21d21695936e66ea762000b64ec95ea0b3e8
Denis Saveliev <96991873+DenHelloWorld@users.noreply.github.com>
2024-08-24 20:34:49 +0300

    Carriages (manager) (#26)
    * fix: fix synch userstate with ls
    
    * chore: add protection against the bug when one of the keys is deleted from ls
    
    * feat: add trigger form
    
    * feat: add demo version of ofrm
    
    * feat: add temporary form
    
    * chore: add CarriageService.createCarriage
    
    * chore: add founded carriage
    
    * feat: add close process
    
    * feat: add hardcode to carriage form
    
    * fix: fix name of carriage
    
    * fix: fix name validator
    
    * feat: add design to carriages
    
    * feat: add update carriage
    
    * chore: process before separation
    
    * refactor: refactor form
    
    * fix: fix header in form
    
    * fix: add carriage in start of list
    
    * fix: fix layout
    
    * refactor: delete logs

--- Train-A/src/app/redux/reducers/carriage.reducer.ts
+++ Train-A/src/app/redux/reducers/carriage.reducer.ts
@@ -1,5 +1,11 @@
 import { createReducer, on } from '@ngrx/store';
-import { loadCarriagesSuccess } from '../actions/carriage.actions';
+import {
+  createCarriageSuccess,
+  hideCarriageForm,
+  loadCarriagesSuccess,
+  showCarriageForm,
+  updateCarriageSuccess,
+} from '../actions/carriage.actions';
 import { CarriageState, initialCarriageState } from '../states/carriage.state';
 
 export const carriageReducer = createReducer(
@@ -9,6 +15,46 @@ export const carriageReducer = createReducer(
     (state, { carriages }): CarriageState => ({
       ...state,
       carriages,
+    }),
+  ),
+  on(
+    showCarriageForm,
+    (state, { carriageCode, mode }): CarriageState => ({
+      ...state,
+      formVisibleForCarriageCode: carriageCode,
+      mode: mode,
+    }),
+  ),
+  on(
+    hideCarriageForm,
+    (state): CarriageState => ({
+      ...state,
+      formVisibleForCarriageCode: null,
+    }),
+  ),
+  on(updateCarriageSuccess, (state, { carriage }) => {
+    const carriageIndex = state.carriages.findIndex(
+      (c) => c.code === carriage.code,
+    );
+    if (carriageIndex !== -1) {
+      const updatedCarriages = [...state.carriages];
+      updatedCarriages[carriageIndex] = {
+        ...updatedCarriages[carriageIndex],
+        ...carriage,
+      };
+      return {
+        ...state,
+        carriages: updatedCarriages,
+        formVisibleForCarriageCode: null,
+      };
+    }
+    return state;
+  }),
+  on(
+    createCarriageSuccess,
+    (state, { carriage }): CarriageState => ({
+      ...state,
+      carriages: [carriage, ...state.carriages],
     }),
   ),
 );
--- Train-A/src/app/redux/states/carriage.state.ts
+++ Train-A/src/app/redux/states/carriage.state.ts
@@ -2,8 +2,12 @@ import { CarriageItem } from '../../admin/models/carriage-item.interface';
 
 export interface CarriageState {
   carriages: CarriageItem[];
+  formVisibleForCarriageCode: string | null;
+  mode: 'create' | 'update';
 }
 
 export const initialCarriageState: CarriageState = {
   carriages: [],
+  formVisibleForCarriageCode: null,
+  mode: 'create',
 };

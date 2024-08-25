7d401a983c8de30ba9e5ba3aa76ceb9dd912942c
Natalia Bandurova <natacsp@mail.ru>
2024-08-25 17:27:50 +0300

    feat: add routes page

--- Train-A/src/app/admin/pages/admin-page/routes-page/routes-page.component.ts
+++ Train-A/src/app/admin/pages/admin-page/routes-page/routes-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CustomButtonComponent } from '../../../../shared/components/custom-button/custom-button.component';
 
 @Component({
   selector: 'app-routes-page',
   standalone: true,
-  imports: [],
+  imports: [CustomButtonComponent],
   templateUrl: './routes-page.component.html',
   styleUrl: './routes-page.component.scss',
 })

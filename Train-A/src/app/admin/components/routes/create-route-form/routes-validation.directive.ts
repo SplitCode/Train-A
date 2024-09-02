a9f54b19e842f41cad898db60704000e113a1cb9
Natalia Bandurova <natacsp@mail.ru>
2024-09-02 12:35:27 +0300

    feat: add errors if number of st or car less 3

--- Train-A/src/app/admin/components/routes/create-route-form/routes-validation.directive.ts
+++ Train-A/src/app/admin/components/routes/create-route-form/routes-validation.directive.ts
@@ -1,14 +1,33 @@
 import { AbstractControl, FormArray, ValidationErrors } from '@angular/forms';
 
+// export function validateRouteForm(
+//   control: AbstractControl,
+// ): ValidationErrors | null {
+//   const stations = control.get('stations') as FormArray;
+//   const carriages = control.get('carriages') as FormArray;
+
+//   if (stations.length < 4 || carriages.length < 4) {
+//     return { minLength: true };
+//   }
+
+//   return null;
+// }
+
 export function validateRouteForm(
   control: AbstractControl,
 ): ValidationErrors | null {
   const stations = control.get('stations') as FormArray;
   const carriages = control.get('carriages') as FormArray;
 
-  if (stations.length < 4 || carriages.length < 4) {
-    return { minLength: true };
+  const errors: ValidationErrors = {};
+
+  if (stations.length < 4) {
+    errors['stationsMinLength'] = true;
   }
 
-  return null;
+  if (carriages.length < 4) {
+    errors['carriagesMinLength'] = true;
+  }
+
+  return Object.keys(errors).length ? errors : null;
 }

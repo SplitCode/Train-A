4581a71aff1f62dcc0482d67df38880f0ebfec5c
Natalia Bandurova <natacsp@mail.ru>
2024-09-03 23:33:16 +0300

    feat: add routes to order page

---
+++ Train-A/src/app/order/pages/order-page/order-page.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+import { OrderPageComponent } from './order-page.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: OrderPageComponent,
+  },
+];

225fc6e47d170c76db271a162cbd3d63557c44cc
Natalia Bandurova <natacsp@mail.ru>
2024-09-04 04:40:58 +0300

    feat: add start and end stations

--- Train-A/src/app/order/components/order-item/order-item.component.ts
+++ Train-A/src/app/order/components/order-item/order-item.component.ts
@@ -5,18 +5,44 @@ import { StationCityByIdPipe } from '../../../home/pipes/station-sity-by-id.pipe
 import { CustomButtonComponent } from '../../../shared/components';
 import { Store } from '@ngrx/store';
 import { orderModal } from '../../../redux/actions/order.actions';
+import { FullTimePipe } from '../../../home/pipes/full-time.pipe';
+import { Observable } from 'rxjs';
+import { GetCityByIDService } from '../../../shared/services/getCityByID.service';
 
 @Component({
   selector: 'app-order-item',
   standalone: true,
-  imports: [CommonModule, StationCityByIdPipe, CustomButtonComponent],
+  imports: [
+    CommonModule,
+    StationCityByIdPipe,
+    CustomButtonComponent,
+    FullTimePipe,
+  ],
   templateUrl: './order-item.component.html',
   styleUrl: './order-item.component.scss',
 })
 export class OrderItemComponent {
   @Input() order!: OrderItem;
 
-  constructor(private store: Store) {}
+  stationStartCity$: Observable<string | undefined> | undefined;
+
+  stationEndCity$: Observable<string | undefined> | undefined;
+
+  constructor(
+    private store: Store,
+    private getCityByIDService: GetCityByIDService,
+  ) {}
+
+  ngOnInit(): void {
+    if (this.order) {
+      this.stationStartCity$ = this.getCityByIDService.getCityByID(
+        this.order.stationStart,
+      );
+      this.stationEndCity$ = this.getCityByIDService.getCityByID(
+        this.order.stationEnd,
+      );
+    }
+  }
 
   public setModalInfo(visible: boolean, orderId: number): void {
     this.store.dispatch(
